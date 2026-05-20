77f2002d2f33972ab5b81db9d8a37d3fac576442
quocvietpham185 <quocvietpham185@gmail.com>
2026-05-20 18:23:33 +0700

    Force Render temp directory to container tmp

--- linkedin_group_crawler/scripts/start_render.sh
+++ linkedin_group_crawler/scripts/start_render.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 set -eu
 
-mkdir -p "${TMPDIR:-/tmp}"
+export TMPDIR=/tmp
+export TEMP=/tmp
+export TMP=/tmp
+
+mkdir -p /tmp
 
 exec uvicorn app.main:app --host "${HOST:-0.0.0.0}" --port "${PORT:-8000}"
